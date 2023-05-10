96ecbb82711bb01a9c5e2b42ac2e8f0a712bb469
reinankaua <69223145+reinankaua@users.noreply.github.com>
2023-05-10 22:42:38 +0000

    Initial commit

---
+++ conta_bancaria.py
@@ -0,0 +1,11 @@
+class Conta:
+  def __init__(self, numero_conta, titular, saldo):
+    self.numero_conta = numero_conta
+    self.titular = titular
+    self.saldo = saldo
+  def depositar(self, numero_conta, novo_valor):
+    if novo_valor > 0:
+      self.saldo += novo_valor
+    else:
+      raise Exception("Valor inválido")
+  def sacar():
