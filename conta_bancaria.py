daf948440c087cbfcfd2b9d084182bf9337b11b5
reinankaua <69223145+reinankaua@users.noreply.github.com>
2023-05-17 21:47:47 +0000

    new commit

--- conta_bancaria.py
+++ conta_bancaria.py
@@ -1,12 +1,35 @@
+import datetime
+
 class Conta:
   def __init__(self, numero_conta, titular, saldo):
     self.numero_conta = numero_conta
     self.titular = titular
     self.saldo = saldo
-  def depositar(self, numero_conta, novo_valor):
+    self.movimentacoes = list()
+    
+  def depositar(self, novo_valor):
     if novo_valor > 0:
       self.saldo += novo_valor
+      self.add_movimentacao("Depósito", novo_valor)
     else:
-      raise Exception("Valor inválido")
-  def sacar():
-    
+      raise ValueError("Valor inválido")
+      
+  def sacar(self, valor):
+    if self.saldo >= valor:
+      self.saldo -= valor
+      self.add_movimentacao("Saque", valor)
+    else:
+      raise ValueError("Saldo insuficiente!")
+      
+  def add_movimentacao(self, acao, valor):
+    data = datetime.datetime.now()
+    data = data.strftime("%x")
+    movimentacao = f'{acao} no valor de R$ {valor} na data {data}'
+    self.movimentacoes.append(movimentacao)
+
+  def extrato(self):
+    print("----------Extrato----------")
+    print(f"Conta: {self.numero_conta}\nTitular: {self.titular}")
+    for movimentacao in self.movimentacoes:
+      print(movimentacao)
+    print(f"---------- Saldo: {self.saldo}")
---
+++ main.py
@@ -0,0 +1,12 @@
+from conta_bancaria import Conta
+
+conta1 = Conta(1, "Reinan", 200)
+conta1.extrato()
+conta1.depositar(200)
+conta1.extrato()
+conta1.depositar(200)
+conta1.extrato()
+conta1.depositar(200)
+conta1.extrato()
+conta1.depositar(200)
+conta1.extrato()
